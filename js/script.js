386aa366b657482807e828988134ed19057d520b
Nicko <nwmariano@gmail.com>
2020-09-10 14:29:57 -0400

    hide otherJobField on page load. It should only be visible when a user selects 'other' from the Job Role select input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+const otherJobField = document.querySelector(".js-other-title");
 
 
 
@@ -11,6 +11,11 @@ init();
 function init(){
 	// Focus on first text input on page
 	document.querySelector("input[type=text]").focus();
+
+	// hide 'other' job role field. It should only be revealed if
+	// a user selects 'other' from the job role dropdown
+	otherJobField.style.display = "none";
+
 }
 
 
